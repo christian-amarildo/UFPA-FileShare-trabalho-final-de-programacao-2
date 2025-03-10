946418af954f083ddeb0000528ccf6f4f4e37761
RogerParrot <rogeriodosanjosbarbosa2004@gmail.com>
2025-03-10 20:03:55 -0300

    Movendo os métodos e atributos da classe Main para a classe Sistema

--- Codigo/FileShare codigo/src/Sistema.java
+++ Codigo/FileShare codigo/src/Sistema.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Sistema implements SystemMethods {
     // Atributo
@@ -45,6 +46,43 @@ public class Sistema implements SystemMethods {
                 System.out.println("Nome: " + user.getNome() + ", Tipo de Usuário: " + user.getClass().getSimpleName());
             }
         }
+    }
+
+    public static void systemRun() {
+        Scanner sc = new Scanner(System.in);
+
+        // Criando alguns usuários para registrar no sistema
+        User aluno = new Aluno("João Silva", "Aluno de Ciência da Computação", new Login("joao@email.com", "senha123", "Seguranca@123"), 12345, Assinatura.PADRAO);
+        User moderador = new Moderador("Maria Souza", "Moderadora do Sistema", new Login("maria@email.com", "senha456", "Seguranca@456"), 2);
+
+        System.out.println("Bem vindo ao sistema!");
+        System.out.println("O que você deseja fazer?");
+        System.out.println("1 - Acessar seus arquivos");
+        System.out.println("2 - Acessar os Rankings");
+        System.out.println("3 - Acessar os grupos");
+
+        int opcao = sc.nextInt();
+
+        // Registrando os usuários no sistema
+        this.userRegistration(aluno);
+        this.userRegistration(moderador);
+
+        switch(opcao) {
+            case 1:
+                this.fileStorage();
+                break;
+            case 2:
+                this.acessRanking();
+                break;
+            case 3:
+                this.acessGroups();
+                break;
+        }
+
+        // Exibindo os usuários registrados
+        this.exibirUsuarios();
+
+        // Chamando os outros métodos do sistema
     }
 
     // Getters e Setters
